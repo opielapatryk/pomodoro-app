39660e05f8b138116586891213051409e6d09482
opielapatryk <xpatrykopiela@gmail.com>
2023-11-06 17:00:43 +0100

    timer works

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Vibration} from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* switch btw 25 and 5 min */}
-      <Text style={{fontSize:50}}>{minutes}:{seconds}</Text> {/* count down second until reach 00:00, then phone buzz */}
-      <View style={{flexDirection:'row'}}>
-        <Button title='Start'></Button>
-        <Button title='Stop'></Button>
-        <Button title='Reset'></Button>
-      </View>
+      <Timer/>  
       <StatusBar style="auto" />
     </View>
   );
@@ -23,8 +18,82 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  rowflex:{
+    flexDirection:'row'
+  },
+  fontfifty:{
+    fontSize:50
+  }
 });
 
-const hours = new Date().getHours();
-const minutes = new Date().getMinutes();
-const seconds = new Date().getSeconds();
+const longerTime = 25*1
+const shorterTime = 5*1
+let newSeconds = longerTime
+
+const Timer = () => {
+  const [getSeconds, setSeconds] = useState(longerTime);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 0) {
+            setIsRunning(false);
+            Vibration.vibrate([500,500,500]);
+            return prevSeconds;
+          }
+          return prevSeconds - 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => {
+      clearInterval(timer);
+    }
+  }, [isRunning]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  }
+
+  const stopTimer = () => {
+    setIsRunning(false);
+  }
+
+  const resetTimer = () => {
+    setSeconds(newSeconds);
+    setIsRunning(false);
+  }
+
+  return (
+    <View>
+      <Text style={styles.fontfifty}>Choose time</Text>
+
+      <View style={styles.rowflex}>
+        <Button title='25' onPress={() => { 
+          setSeconds(longerTime)
+          newSeconds = longerTime
+          }}></Button>
+        <Button title='5' onPress={() => { 
+          setSeconds(shorterTime) 
+          newSeconds = shorterTime 
+          }}></Button>
+      </View>
+
+      <Text style={styles.fontfifty}>
+        {(Math.floor(getSeconds / 60)).toString().padStart(2, '0')}:{(getSeconds % 60).toString().padStart(2, '0')}
+      </Text>
+
+      <View style={styles.rowflex}>
+        <Button title='Start' color={'green'} onPress={startTimer}></Button>
+        <Button title='Stop' color={'red'} onPress={stopTimer}></Button>
+        <Button title='Reset' color={'orange'} onPress={resetTimer}></Button>
+      </View>
+    </View>
+  )
+}
